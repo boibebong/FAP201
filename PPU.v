8d2e25ebe301974192b6d2c832da2c1194644f95
boibebong <duyminhnguyen2020@gmail.com>
2026-03-22 21:06:38 +0700

    Update PPU.v

--- PPU.v
+++ PPU.v
@@ -1,15 +1,9 @@
-module PPU(
-    input [7:0] c,
-    input [7:0] d,
-    output [63:0] pp_raw // Tr? v? 64 bits tích riêng l?
+module PPU_signed_4bit(
+    input  wire signed [3:0] a,
+    input  wire signed [3:0] b,
+    output wire signed [7:0] mult_result
 );
-    genvar i, j;
-    generate
-        for (i=0; i<8; i=i+1) begin : row
-            for (j=0; j<8; j=j+1) begin : col
-                // C?ng NAND t?o tích riêng l? (Partial Product)
-                and(pp_raw[i*8 + j], c[i], d[j]);
-            end
-        end
-    endgenerate
+
+    assign mult_result = a * b;
+
 endmodule
