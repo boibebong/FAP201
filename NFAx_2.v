1c0f5f90c208532e497d45a3139e4eb98422154c
boibebong <duyminhnguyen2020@gmail.com>
2026-03-23 16:01:23 +0700

    Update NFAx_2.v

--- NFAx_2.v
+++ NFAx_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/04/2026 10:33:04 PM
-// Design Name: 
-// Module Name: NFAx_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module NFAx(
     input a,
     input b,
@@ -26,9 +6,6 @@ module NFAx(
     output sum,
     output carry
 );
-    // S? d?ng XOR cho Sum ?? tránh l?i bão hòa (Saturation Error) khi c = 0
     assign sum   = a ^ b ^ c; 
-    
-    // Carry x?p x? ?? ti?t ki?m c?ng logic
     assign carry = a & b; 
 endmodule
